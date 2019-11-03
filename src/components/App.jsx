14643445b55fa40270f198f46ed4610775632a8e
Daniël Botha <botha.daniel1@gmail.com>
2019-11-03 21:53:53 +0200

    chore: setup boilerplate repo

---
+++ src/components/App.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
+
+const RootStyles = createGlobalStyle`
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+    monospace;
+}
+`;
+
+const AppStyle = styled.div`
+  text-align: center;
+`;
+
+const Header = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+function App() {
+  return (
+    <>
+      <RootStyles />
+      <AppStyle>
+        <Header>
+          <h1>Hello, World!</h1>
+        </Header>
+      </AppStyle>
+    </>
+  );
+}
+
+export default App;
